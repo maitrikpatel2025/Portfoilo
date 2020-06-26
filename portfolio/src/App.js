4690faee475a62039af90462b88c08b29995a8f7
Maitrik Patel <59853047+maitrikpatel2025@users.noreply.github.com>
2020-06-26 19:11:26 -0400

    porfoilo

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import TopNav from './Components/TopNav/TopNav';
-import Home from './Components/Home/Home';
-import Scroll from './Components/Scroll/Scroll';
-
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import TopNav from "./Components/TopNav/TopNav";
+import Home from "./Components/Home/Home";
+import Scroll from "./Components/Scroll/Scroll";
+import About from "./Components/About/About";
+import SideNav from "./Components/SideNav/SideNav";
 
 function App() {
-	return (
-		<div className="App">
-			<Router>
-				<Switch>
-				<Route path="/" component={TopNav} />
-				</Switch>
-				<Route path="/" component={Home}/>
-			</Router>
-			<Scroll/>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+		  <Route path="/" component={TopNav} />
+		  <Route path="/" component={SideNav} />
+        </Switch>
+        <div className="Page">
+          <switch>
+            <Route path="/" component={Home} />
+            <Route path="/about" component={About} />
+          </switch>
+        </div>
+      </Router>
+      <Scroll />
+    </div>
+  );
 }
 
 export default App;
---
+++ portfolio/src/Components/About/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import "./About.css";
+
+const About = () => {
+    return ( <div className="About">
+    maif
+    </div> );
+}
+ 
+export default About;
